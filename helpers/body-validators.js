f4d40e6627161bee63c1c444b98bf1f2a828547a
wawrzynmc <karol.wawrzenczyk@gmail.com>
2020-10-11 12:19:47 +0200

    :bug: Add check for routes consist 'email'

--- helpers/body-validators.js
+++ helpers/body-validators.js
@@ -2,42 +2,45 @@
 const { check } = require('express-validator');
 
 exports.signupValidator = [
-    check('name', 'Name is required.')
-        .trim()
+	check('name', 'Name is required.')
+		.trim()
 		.notEmpty()
-        .withMessage('Name is required.')
-        .bail()
+		.withMessage('Name is required.')
+		.bail()
 		.isLength({
 			min: 4,
 			max: 32,
 		})
-        .withMessage('Name must be between 4 to 32 characters.')
+		.withMessage('Name must be between 4 to 32 characters.')
 		.bail()
-		.custom((value) => (/^[A-Za-z]+$/.test(value)))
+		.custom((value) => /^[A-Za-z]+$/.test(value))
 		.withMessage('Name has to contain only alphabetical characters.'),
 	check('email')
 		.normalizeEmail()
 		.isEmail()
-        .withMessage('Invalid email address.'),
+		.withMessage('Invalid email address.'),
 	check('password1')
 		.notEmpty()
-        .withMessage('Password is required.')
-        .bail()
+		.withMessage('Password is required.')
+		.bail()
 		.isLength({
 			min: 6,
 			max: 100,
 		})
-        .withMessage('Password must contain at least 6 characters.'),
+		.withMessage('Password must contain at least 6 characters.'),
 	check('password2')
 		.notEmpty()
-        .withMessage('Password confirmation is required.')
-        .bail()
-        .custom((value, { req }) => value === req.body.password1)
-        .withMessage('Password and password confirmation have to match.')
+		.withMessage('Password confirmation is required.')
+		.bail()
+		.custom((value, { req }) => value === req.body.password1)
+		.withMessage('Password and password confirmation have to match.'),
 ];
 
 exports.signinValidator = [
-	check('email').isEmail().withMessage('Must be a valid email address'),
+	check('email')
+		.normalizeEmail()
+		.isEmail()
+		.withMessage('Must be a valid email address'),
 	check('password', 'password is required').notEmpty(),
 	check('password')
 		.isLength({
@@ -50,8 +53,7 @@ exports.signinValidator = [
 
 exports.emailValidator = [
 	check('email')
-		.not()
-		.isEmpty()
+		.normalizeEmail()
 		.isEmail()
 		.withMessage('Must be a valid email address'),
 ];
@@ -63,4 +65,3 @@ exports.resetPasswordValidator = [
 		.isLength({ min: 6 })
 		.withMessage('Password must be at least  6 characters long'),
 ];
-
