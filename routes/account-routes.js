9a217e3d5b51f4c6595ac1f0dec5707de616a559
wawrzynmc <karol.wawrzenczyk@gmail.com>
2020-11-20 12:34:28 +0100

    :recycle: refactore code

--- routes/account-routes.js
+++ routes/account-routes.js
@@ -226,31 +226,3 @@ router.put(
 
 // -- export
 module.exports = router;
-
-/**
- * @swagger
- * tags:
- *   name: Users
- *   description: User management
- */
-/**
- * @swagger
- * path:
- *  /users/:
- *    post:
- *      summary: Create a new user
- *      tags: [Users]
- *      requestBody:
- *        required: true
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/User'
- *      responses:
- *        "200":
- *          description: A user schema
- *          content:
- *            application/json:
- *              schema:
- *                $ref: '#/components/schemas/User'
- */
